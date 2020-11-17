7ca01ccb819cda2d757f7cafcb98df641cebb797
lbavsc <471242574@qq.com>
2020-11-17 15:20:53 +0800

    studeng头文件

---
+++ include/student_pub.h
@@ -0,0 +1,26 @@
+/*************************************************************************
+*File Name: student.h
+*Author: lbavsc
+*471242574@qq.com
+*Created Time: 2020年11月12日 星期四 11时02分57秒
+ ************************************************************************/
+#pragma once
+
+#define STUDENT_ALL 0
+
+#define STUDENT_ID 1
+
+#define STUDENT_NAME 2
+
+void addStudent();
+
+void deleteStudent();
+
+void modifyStudent(int type);
+
+void viewStudent(int type);
+
+
+
+
+
